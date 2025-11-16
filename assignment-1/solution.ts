58fccd0b37a5023323ca80899f52382d461a2855
Asraful Islam <gdmamun@yahoo.com>
2025-11-17 00:22:23 +0100

    3rd is done

--- assignment-1/solution.ts
+++ assignment-1/solution.ts
@@ -1,1 +1,40 @@
 //problem 1
+
+type FormatValue = number | string | boolean;
+const formatValue = (value: FormatValue): FormatValue => {
+    if (typeof value === 'string') {
+        return value.toUpperCase();
+    } else if (typeof value === 'number') {
+        return value*10;
+    } else {
+        return !value;
+    }
+}
+
+//problem 2
+
+type valueType = string | any[];
+
+const getLength = (value: valueType): number => {
+    if (typeof value === "string") {
+        return value.length;
+    } else if (Array.isArray(value)) {
+        return value.length;
+    }
+    return 0;
+}
+
+//problem 3
+
+class Person {
+    name: string;
+    age: number;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    }
+    getDetails(): string {
+        return `Name: ${this.name}, Age: ${this.age}`;
+    }
+}
