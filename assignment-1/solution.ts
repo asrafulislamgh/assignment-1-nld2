371239dfc0cf33a97d8589737407a725c019017a
Asraful Islam <gdmamun@yahoo.com>
2025-11-17 02:42:26 +0100

    done

--- assignment-1/solution.ts
+++ assignment-1/solution.ts
@@ -1,4 +1,5 @@
-//problem 1
+
+
 
 type FormatValue = number | string | boolean;
 const formatValue = (value: FormatValue): FormatValue => {
@@ -11,7 +12,7 @@ const formatValue = (value: FormatValue): FormatValue => {
     }
 }
 
-//problem 2
+
 
 type valueType = string | any[];
 
@@ -24,7 +25,7 @@ const getLength = (value: valueType): number => {
     return 0;
 }
 
-//problem 3
+
 
 class Person {
     name: string;
@@ -35,6 +36,94 @@ class Person {
         this.age = age;
     }
     getDetails(): string {
-        return `Name: ${this.name}, Age: ${this.age}`;
+        return `'Name: ${this.name}, Age: ${this.age}'`;
     }
+}
+
+
+
+
+type bookType = {
+    title : string;
+    rating : number;
+}
+const filterByRating = (books: bookType[]) : bookType[] => {
+    const booksAboveRatingFour = books.filter((book) => book.rating >= 4);
+    return booksAboveRatingFour;
+}
+
+
+
+interface User {
+    id: number;
+    name: string;
+    email: string;
+    isActive: boolean;
+}
+const filterActiveUsers = (users: User[]) : User[] => {
+    const activeUser = users.filter((user) => user.isActive === true);
+    return activeUser;
+}
+
+
+
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
+}
+
+const printBookDetails = (book: Book): string => {
+    const bookDetails = `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${book.isAvailable ? 'Yes' : 'No'}`;
+    console.log(bookDetails);
+    return bookDetails;
+}
+
+
+
+type arrayValue = number | string;
+const getUniqueValues = (arr1: arrayValue[], arr2: arrayValue[]): arrayValue[] => {
+    const combinedArray: arrayValue[] = [];
+    for (const value of arr1) {
+        combinedArray.push(value);
+    }
+    for (const value of arr2) {
+        combinedArray.push(value);
+    }
+
+    const uniqueValues: arrayValue[] = [];
+    for (const value of combinedArray) {
+        let isUnique = true;
+        
+        for (const uniqueValue of uniqueValues) {
+            if (value === uniqueValue) {
+                isUnique = false;
+                break;
+            }
+        }
+        if (isUnique) {
+            uniqueValues.push(value);
+        }
+    }
+    return uniqueValues;
+}
+
+
+
+interface Product {
+    name: string;
+    price: number;
+    quantity: number;
+    discount?: number;
+}
+const calculateTotalPrice = (products: Product[]): number => {
+    if (products.length === 0) {
+        return 0;
+    }
+    let totalPrice = 0;
+    products.map((product) => {
+    totalPrice = totalPrice + (product.price * product.quantity) - (product.discount ? (product.price * product.quantity * product.discount/100) : 0);   
+    })
+    return totalPrice;
 }
